43e5f44822d10c89bb76f02e6ab9834b60c54634
Marco Carandente <marco.carandente@gmail.com>
2024-03-20 21:52:33 +0200

    Adding Swiftui preview
    kept you waiting, huh?

---
+++ Life2Gpx/Preview.swift
@@ -0,0 +1,24 @@
+//
+//  Preview.swift
+//  Life2Gpx
+//
+//  Created by Marco Carandente on 20.3.2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
